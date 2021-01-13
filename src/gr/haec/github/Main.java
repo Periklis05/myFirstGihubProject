cbbee56697d2857b997af5f736dd80662b8d08ff
Periklis05 <periklisrevelos@gmail.com>
2021-01-13 20:27:02 +0200

    ammend comment

--- src/gr/haec/github/Main.java
+++ src/gr/haec/github/Main.java
@@ -3,6 +3,6 @@ package gr.haec.github;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("My First one!!!");
+	System.out.println("My First one!!!!!!!!!!!!!!!!!!!");
     }
 }
